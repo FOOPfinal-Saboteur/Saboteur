ddd8eaf189b363bbd1f6a41d00c9d44b03b426b2
施秉志 <b03902039@ntu.edu.tw>
2016-01-13 23:29:54 +0800

    Jp try

--- GUI/JPanelTutorial.java
+++ GUI/JPanelTutorial.java
@@ -6,17 +6,25 @@ import java.io.File;
 import java.io.IOException;
 
 public class JPanelTutorial extends JPanel{
-	BufferedImage image1, image2;
+	BufferedImage image1, image2, bgimage;
+	int card_width = 65;
+	int card_length = 100;
 
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
 			@Override
 			public void run() {
+				Dimension dim = new Dimension((707*14)/10,(512*14)/10 +22);
 				JFrame frame = new JFrame();
 				frame.setTitle("GUI test");
 				frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 				frame.add(new JPanelTutorial());
-				frame.setSize(960,720);
+				frame.setSize((707*14)/10 , (512*14)/10);
+				//frame.setPreferredSize(dim);
+				frame.setMinimumSize(dim);
+				frame.setMaximumSize(dim);
+				frame.setResizable(false);
+				//frame.pack();
 				//frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
 				frame.setVisible(true);
 			}
@@ -25,15 +33,26 @@ public class JPanelTutorial extends JPanel{
 
 	public JPanelTutorial(){
 		try {
-			image1 = ImageIO.read(new File(System.getProperty("user.dir")+"/img/fix_pick.png"));
+			bgimage = ImageIO.read(new File(System.getProperty("user.dir")+"/img/Sab-Map.jpg"));
+			image1 = ImageIO.read(new File(System.getProperty("user.dir")+"/img/intersection.png"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 	
 	public void paintComponent(Graphics g){
-		super.paintComponents(g);
-		g.drawImage(image1,200,200,117,168,null);
+		//super.paintComponents(g);
+		g.drawImage(bgimage,0,0,(707*14)/10,(512*14)/10,null);		
+		g.drawImage(image1,70,196,card_width,card_length,null);
+		g.drawImage(image1,70+75*1,196,card_width,card_length,null);
+		g.drawImage(image1,70+75*2,196,card_width,card_length,null);
+		g.drawImage(image1,70+75*3,196,card_width,card_length,null);
+		g.drawImage(image1,70+75*4,196,card_width,card_length,null);
+		g.drawImage(image1,70+75*5,196,card_width,card_length,null);
+		g.drawImage(image1,70+75*6,196,card_width,card_length,null);
+		g.drawImage(image1,70+75*7,196,card_width,card_length,null);
+		g.drawImage(image1,70+75*8,196,card_width,card_length,null);
+
 		// g.setColor(Color.RED);
 		// g.fillRect(10, 10, 100, 50);
 	}
