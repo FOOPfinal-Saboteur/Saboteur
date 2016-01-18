f5995dfb5f0d1806be9c22dc9c9bfded03bd9a57
施秉志 <b03902039@ntu.edu.tw>
2016-01-18 19:52:32 +0800

    affect status func

--- GUI/sample/MainController.java
+++ GUI/sample/MainController.java
@@ -35,27 +35,24 @@ public class MainController implements Initializable {
     /* right col */
     @FXML private VBox rightcol;
 
-    @FXML private Label p1_id;
-    @FXML private Label p2_id;
-    @FXML private Label p3_id;
-    @FXML private Label p4_id;
-    @FXML private Label p5_id;
-    @FXML private Label p6_id;
-    @FXML private Label p7_id;
-    @FXML private Label p8_id;
-    @FXML private Label p9_id;
-    @FXML private Label p10_id;
+    @FXML private Label p1_id; @FXML private Label p2_id; @FXML private Label p3_id; @FXML private Label p4_id;
+    @FXML private Label p5_id; @FXML private Label p6_id; @FXML private Label p7_id; @FXML private Label p8_id;
+    @FXML private Label p9_id; @FXML private Label p10_id;
 
-    @FXML private HBox s1_id;
-    @FXML private HBox s2_id;
-    @FXML private HBox s3_id;
-    @FXML private HBox s4_id;
-    @FXML private HBox s5_id;
-    @FXML private HBox s6_id;
-    @FXML private HBox s7_id;
-    @FXML private HBox s8_id;
-    @FXML private HBox s9_id;
-    @FXML private HBox s10_id;
+    @FXML private HBox s1_id; @FXML private HBox s2_id; @FXML private HBox s3_id; @FXML private HBox s4_id;
+    @FXML private HBox s5_id; @FXML private HBox s6_id; @FXML private HBox s7_id; @FXML private HBox s8_id;
+    @FXML private HBox s9_id; @FXML private HBox s10_id;
+
+    @FXML private ImageView item1_1; @FXML private ImageView item1_2; @FXML private ImageView item1_3;
+    @FXML private ImageView item2_1; @FXML private ImageView item2_2; @FXML private ImageView item2_3;
+    @FXML private ImageView item3_1; @FXML private ImageView item3_2; @FXML private ImageView item3_3;
+    @FXML private ImageView item4_1; @FXML private ImageView item4_2; @FXML private ImageView item4_3;
+    @FXML private ImageView item5_1; @FXML private ImageView item5_2; @FXML private ImageView item5_3;
+    @FXML private ImageView item6_1; @FXML private ImageView item6_2; @FXML private ImageView item6_3;
+    @FXML private ImageView item7_1; @FXML private ImageView item7_2; @FXML private ImageView item7_3;
+    @FXML private ImageView item8_1; @FXML private ImageView item8_2; @FXML private ImageView item8_3;
+    @FXML private ImageView item9_1; @FXML private ImageView item9_2; @FXML private ImageView item9_3;
+    @FXML private ImageView item10_1; @FXML private ImageView item10_2; @FXML private ImageView item10_3;
 
     @FXML private Label chat;
 
@@ -72,6 +69,8 @@ public class MainController implements Initializable {
     }
     public MainController() {
     }
+
+    /* init game */
 
     @FXML
     protected void setStatus(int pnum, ArrayList<String> names){
@@ -97,6 +96,8 @@ public class MainController implements Initializable {
         /* Init */
         this.round.setText(rnd+"/3");
     }
+
+    /* chatroom */
 
     @FXML
     public boolean chatIsOpen(){
@@ -124,8 +125,6 @@ public class MainController implements Initializable {
     @FXML
     public void chatClosePrs(KeyEvent event){ if(event.getCode() == KeyCode.TAB) chatClose(); }
 
-
-
     @FXML
     public void chatMsgSend(ActionEvent event){
         if(!this.chatinput.getCharacters().toString().trim().equals("")) {
@@ -144,6 +143,41 @@ public class MainController implements Initializable {
         }else if(event.getCode() == KeyCode.TAB) {
             chatClose();
         }
+    }
+
+    /* Affect player's status */
+    @FXML
+    public void attack(int playerid, int item){
+        ImageView[] st_id1 = new ImageView[]{item1_1, item1_2, item1_3};
+        ImageView[] st_id2 = new ImageView[]{item2_1, item2_2, item2_3};
+        ImageView[] st_id3 = new ImageView[]{item3_1, item3_2, item3_3};
+        ImageView[] st_id4 = new ImageView[]{item4_1, item4_2, item4_3};
+        ImageView[] st_id5 = new ImageView[]{item5_1, item5_2, item5_3};
+        ImageView[] st_id6 = new ImageView[]{item6_1, item6_2, item6_3};
+        ImageView[] st_id7 = new ImageView[]{item7_1, item7_2, item7_3};
+        ImageView[] st_id8 = new ImageView[]{item8_1, item8_2, item8_3};
+        ImageView[] st_id9 = new ImageView[]{item9_1, item9_2, item9_3};
+        ImageView[] st_id10 = new ImageView[]{item10_1, item10_2, item10_3};
+        ImageView[][] st_id = new ImageView[][]{st_id1, st_id2, st_id3, st_id4, st_id5, st_id6, st_id7, st_id8, st_id9, st_id10};
+
+        st_id[playerid-1][item-1].setVisible(true);
+    }
+
+    @FXML
+    public void rescue(int playerid, int item){
+        ImageView[] st_id1 = new ImageView[]{item1_1, item1_2, item1_3};
+        ImageView[] st_id2 = new ImageView[]{item2_1, item2_2, item2_3};
+        ImageView[] st_id3 = new ImageView[]{item3_1, item3_2, item3_3};
+        ImageView[] st_id4 = new ImageView[]{item4_1, item4_2, item4_3};
+        ImageView[] st_id5 = new ImageView[]{item5_1, item5_2, item5_3};
+        ImageView[] st_id6 = new ImageView[]{item6_1, item6_2, item6_3};
+        ImageView[] st_id7 = new ImageView[]{item7_1, item7_2, item7_3};
+        ImageView[] st_id8 = new ImageView[]{item8_1, item8_2, item8_3};
+        ImageView[] st_id9 = new ImageView[]{item9_1, item9_2, item9_3};
+        ImageView[] st_id10 = new ImageView[]{item10_1, item10_2, item10_3};
+        ImageView[][] st_id = new ImageView[][]{st_id1, st_id2, st_id3, st_id4, st_id5, st_id6, st_id7, st_id8, st_id9, st_id10};
+
+        st_id[playerid-1][item-1].setVisible(false);
     }
 
 }
