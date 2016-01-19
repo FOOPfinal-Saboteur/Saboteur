e9bc3440a008380c92f856f61dd97d355d453d13
施秉志 <b03902039@ntu.edu.tw>
2016-01-19 11:40:20 +0800

    main probably done

---
+++ main/sample/Card.java
@@ -0,0 +1,143 @@
+package sample;
+import java.lang.*;
+import java.util.*;
+import java.io.*;
+
+public class Card{
+	/* Member variable */
+	protected boolean is_function;
+	protected boolean is_road;
+	protected boolean is_rotate;
+	private String my_item;
+	private String my_type;
+	/* Constructor */
+	public Card(){
+		is_function = false;
+		is_road = false;
+		is_rotate = false;
+	}
+	public Card(String type, String item){
+		if(type.equals("fix")||
+		   type.equals("break")||
+		   type.equals("map")||
+		   type.equals("collapse")
+		){
+				is_road = false;
+				is_function = true;
+				is_rotate = false;
+				my_item = new String(item);
+				my_type = new String(type);
+		}
+		else
+			System.out.println("[ERROR] WTF do you want?? You bitch");
+		
+	}
+	public Card(String cmd){
+		if(cmd.equals("intersection")||
+		   cmd.equals("longT")||
+		   cmd.equals("shortT")||
+		   cmd.equals("longI")||
+		   cmd.equals("shortI")||
+		   cmd.equals("LeftTop")||
+		   cmd.equals("RightTop")||
+		   cmd.equals("fullblock")||
+		   cmd.equals("longTblock")||
+		   cmd.equals("shortTblock")||
+		   cmd.equals("LeftTopblock")||
+		   cmd.equals("RightTopblock")||
+		   cmd.equals("longIblock")||
+		   cmd.equals("shortIblock")||
+		   cmd.equals("longDeadEnd")||
+		   cmd.equals("shortDeadEnd")
+		   ){
+			is_road = true;
+			is_function = false;
+			is_rotate = false;
+			my_type = new String(cmd);
+		}
+		else
+			System.out.println("[ERROR] WTF do you want?? You bastard");
+	
+	}
+	public Card(String cmd, boolean rotate){
+		if(cmd.equals("intersection")||
+		   cmd.equals("longT")||
+		   cmd.equals("shortT")||
+		   cmd.equals("longI")||
+		   cmd.equals("shortI")||
+		   cmd.equals("LeftTop")||
+		   cmd.equals("RightTop")||
+		   cmd.equals("fullblock")||
+		   cmd.equals("longTblock")||
+		   cmd.equals("shortTblock")||
+		   cmd.equals("LeftTopblock")||
+		   cmd.equals("RightTopblock")||
+		   cmd.equals("longIblock")||
+		   cmd.equals("shortIblock")||
+		   cmd.equals("longDeadEnd")||
+		   cmd.equals("shortDeadEnd")
+		   ){
+			is_road = true;
+			is_function = false;
+			is_rotate = rotate;
+			my_type = new String(cmd);
+		}
+		else
+			System.out.println("[ERROR] WTF do you want?? You bastard");
+	
+	}
+	public Card(Card card){
+		if(card.IsFunction()){
+			is_road = false;
+			is_function = true;
+			is_rotate = false;
+			my_type = card.getType();
+			my_item = card.getItem();
+		}
+		if(card.IsRoad()){
+			is_road =true;
+			is_function = false;
+			is_rotate = card.IsRotate();
+			my_type = card.getType();
+		}
+//		System.out.println(my_type +"  How\n");
+	}
+	/* Accessor */
+	public FunctionCard Function(){
+		if(!is_function){
+			System.out.println("[ERROR] This is not a function card!! You asshole!!");
+			return null;
+		}
+		FunctionCard toReturn = new FunctionCard(my_type,my_item);
+		return toReturn;	
+	} 
+	public RoadCard Road(){
+		if(!is_road){
+			System.out.println("[ERROR] This is not a road card!! You whore!!");
+			return null;
+		}
+		RoadCard toReturn = new RoadCard(my_type,is_rotate);
+		return toReturn;
+	
+	}
+	public boolean rotateCard(){
+		if(is_function)
+			return false;
+		is_rotate = !is_rotate;
+		return true;
+	}
+	public String getItem(){return new String(my_item);}
+	public String getType(){return new String(my_type);}
+	public boolean IsFunction(){return is_function;}
+	public boolean IsRoad(){return is_road;}
+	public boolean IsRotate(){return is_rotate;};
+	/* Method */ 
+	public String toString(){
+		if(is_function)
+			return new String(new FunctionCard(my_type,my_item).toString());
+		else if(is_road)
+			return new String(new RoadCard(my_type,is_rotate).toString());
+		else
+			return new  String("Empty");
+	}
+}
---
+++ main/sample/Map.java
@@ -0,0 +1,405 @@
+package sample;
+import java.lang.*;
+import java.util.*;
+import java.io.*;
+
+class Position{
+
+	/* Member variable */
+	private boolean haveCard;
+	private boolean isCandidate;
+	/* for destination */
+		private boolean isDestination;
+		private boolean isGold;
+		private boolean flipped;
+	private RoadCard card;
+
+	/* Constructor */
+	public Position(){
+		haveCard = false;
+		isCandidate = false;
+		isDestination = false;
+		isGold = false;
+		card = null;
+	}
+
+	/* Accessor */
+	public boolean getHaveCard(){ return haveCard; }
+	public boolean getIsCandidate(){ return isCandidate; }
+	public boolean getIsDestination(){return isDestination;}
+	public boolean getIsGold(){return isGold;}
+	public boolean getBind(int n){ return card.getBind(n); }
+	public boolean getBindToAny(){
+		for(int i = 0; i < 4; i ++)
+			if(card.getBind(i))
+				return true;
+		return false;
+	}
+	public boolean getConnect(int n){ return card.getConnect(n);}
+	public boolean getIsBlock(){ return card.isBlock();}
+	public boolean getIsFlipped(){ return flipped; }
+
+	/* Mutator */
+	public boolean setCard(RoadCard c){
+		if(haveCard){
+			System.out.println("Occupied");
+			return false;
+		}
+		card = c;
+		haveCard = true;
+		isCandidate = false;
+		return true;
+	}
+
+	public boolean removeCard(){
+		if(!haveCard)
+			return false;
+		card = null;
+		haveCard = false;
+		isCandidate = true;
+		return true;
+	}
+
+	public void setHaveCard(boolean b){ haveCard = b; }
+	public void setDestination(boolean d,boolean g,boolean f){isDestination = d; isGold = g; flipped = f;}
+	public void setFlipped(boolean f){flipped = f;}
+	public void setCandidate(boolean b){ isCandidate = b;/* System.out.println("YEAHYEAHYEAH // "+b);*/}
+}
+
+public class Map{
+
+	/* Member variable */
+		/* [x][y], x:horizontal, y:vertical */
+		/* CTS: connected to source */
+	private Position[][] pos = new Position[10][5];
+
+	private boolean[][] vSideCTS = new boolean[11][5]; // vertical side 
+	private boolean[][] hSideCTS = new boolean[10][6]; // horizontal side
+	
+	private boolean[][] traced = new boolean[10][5]; // for spread
+
+
+	/* Constructor */
+	public Map(){
+		for(int i = 0; i < 10; i++)
+			for(int j = 0; j < 5; j++)
+				pos[i][j] = new Position();
+		for(int i = 0; i < 11; i++)		
+			for(int j = 0; j < 5; j++)
+				vSideCTS[i][j] = false;
+		for(int i = 0; i < 10; i++)		
+			for(int j = 0; j < 6; j++)
+				hSideCTS[i][j] = false;
+		/* set source */
+		RoadCard source = new RoadCard("intersection");
+		pos[1][2].setCard(source);
+		vSideCTS[1][2] = true; // Left 
+		vSideCTS[2][2] = true; // Right
+		hSideCTS[1][2] = true; // Bottom
+		hSideCTS[1][3] = true; // Top
+		/* set first candidates */
+		pos[1][1].setCandidate(true); // Bottom
+		pos[1][3].setCandidate(true); // Top
+		pos[0][2].setCandidate(true); // Left
+		pos[2][2].setCandidate(true); // Right
+		/* set destinations*/
+		pos[9][0].setDestination(true,false,false); // Bottom
+		pos[9][2].setDestination(true,false,false); // Middle
+		pos[9][4].setDestination(true,false,false); // Top
+		// set destination have card
+		pos[9][0].setHaveCard(true); // Bottom
+		pos[9][2].setHaveCard(true); // Middle
+		pos[9][4].setHaveCard(true); // Top
+		Random ran = new Random();
+		pos[9][ran.nextInt(3)*2].setDestination(true, true,false); // TMB choose one
+	}
+
+	/* Accessor */
+	public boolean haveGold(int y){
+		return pos[9][y].getIsGold();
+	}
+
+	public boolean isFlipped(int y){
+		return pos[9][y].getIsFlipped();
+	}
+
+	/* Method */ 
+	public boolean receiveCard(Card card,int x,int y){
+		if(card.IsFunction()){
+			if(card.Function().isCollapse())
+				return breakRoad(x,y);
+		}
+		else if(card.IsRoad()){
+			return placeRoad(card.Road(),x,y);
+		}
+		else{
+			System.out.println("Invalid Card");
+			return false;
+		}
+		return false;
+	}
+	public boolean placeRoad(RoadCard c, int x, int y){
+		/* check position index */
+		if(x >= 10 || x < 0 || y >= 5 || y < 0){
+			System.out.println("No such position");
+			return false;
+		}
+		/* check candidate */
+		if(!pos[x][y].getIsCandidate()){
+			System.out.println("Not candidate");
+			return false;
+		}
+		/* check bind */
+		if(x > 0 && pos[x-1][y].getHaveCard()){ // Left
+			if(pos[x-1][y].getBind(3) != c.getBind(1)){
+				System.out.println("Not matched");
+				return false;
+			}
+		}else if(x < 9 && pos[x+1][y].getHaveCard()){ // Right
+			if(x == 8 && (y == 0 || y == 2 || y == 4)){
+			}else if(pos[x+1][y].getBind(1) != c.getBind(3)){
+				System.out.println("Not matched");
+				return false;
+			}
+		}else if(y > 0 && pos[x][y-1].getHaveCard()){ // Bottom
+			if(x == 9){
+			}else if(pos[x][y-1].getBind(0) != c.getBind(2)){
+				System.out.println("Not matched");
+				return false;
+			}
+		}else if(y < 4 && pos[x][y+1].getHaveCard()){ // Top
+			if(x == 9){
+			}else if(pos[x][y+1].getBind(2) != c.getBind(0)){
+				System.out.println("Not matched");
+				return false;
+			}
+		}
+		pos[x][y].setCard(c);
+		/* update CTS & candidates*/
+		traceInit();
+		spread(x, y);
+
+		if(x == 8 && y == 4){
+			if(c.getBind(3) && vSideCTS[9][4]) {
+				pos[9][4].setFlipped(true);
+			}
+		}else if(x == 8 && y == 2){
+			if(c.getBind(3) && vSideCTS[9][2]) {
+				pos[9][2].setFlipped(true);
+			}
+		}else if(x == 8 && y == 0){
+			if(c.getBind(3) && vSideCTS[9][0]) {
+				pos[9][0].setFlipped(true);
+			}
+		}else if(x == 9 && y == 3){
+			if(c.getBind(0) && hSideCTS[9][4]) {
+				pos[9][4].setFlipped(true);
+			}
+			if(c.getBind(2) && hSideCTS[9][3]) {
+				pos[9][2].setFlipped(true);
+			}
+		}else if(x == 9 && y == 1){
+			if(c.getBind(0) && vSideCTS[9][2]) {
+				pos[9][2].setFlipped(true);
+			}
+			if(c.getBind(2) && vSideCTS[9][1]) {
+				pos[9][0].setFlipped(true);
+			}
+		}
+		
+		return true;
+	}
+
+	protected void traceInit(){
+		for(int i = 0; i < 10; i++)
+			for(int j = 0; j < 5; j++)
+				traced[i][j] = false;
+	}
+
+	protected void spread(int x, int y){
+		if(traced[x][y]) 
+			return;		
+		if(pos[x][y].getConnect(0)){
+			if(vSideCTS[x][y])
+				hSideCTS[x][y+1] = true;
+			if(hSideCTS[x][y+1])
+				vSideCTS[x][y] = true;
+		}
+		if(pos[x][y].getConnect(1)){
+			if(vSideCTS[x][y])
+				hSideCTS[x][y] = true;
+			if(hSideCTS[x][y])
+				vSideCTS[x][y] = true;
+		}
+		if(pos[x][y].getConnect(2)){
+			if(vSideCTS[x+1][y])
+				hSideCTS[x][y] = true;
+			if(hSideCTS[x][y])
+				vSideCTS[x+1][y] = true;
+		}
+		if(pos[x][y].getConnect(3)){
+			if(vSideCTS[x+1][y])
+				hSideCTS[x][y+1] = true;
+			if(hSideCTS[x][y+1])
+				vSideCTS[x+1][y] = true;
+		}
+		if(pos[x][y].getConnect(4)){
+			if(hSideCTS[x][y])
+				hSideCTS[x][y+1] = true;
+			if(hSideCTS[x][y+1])
+				hSideCTS[x][y] = true;
+		}
+		if(pos[x][y].getConnect(5)){
+			if(vSideCTS[x][y])
+				vSideCTS[x+1][y] = true;
+			if(vSideCTS[x+1][y])
+				vSideCTS[x][y] = true;
+		}
+
+		traced[x][y] = true;
+
+		if(x > 0 && !pos[x-1][y].getHaveCard()){ // Left
+			if(vSideCTS[x][y])
+				pos[x-1][y].setCandidate(true);
+		}
+		if(x < 8 && !pos[x+1][y].getHaveCard()){ // Right
+			if(vSideCTS[x+1][y])
+				pos[x+1][y].setCandidate(true);
+		}
+		if(x == 8 && (y == 1 || y == 3)
+				&& !pos[x+1][y].getHaveCard()){ // Right
+			if(vSideCTS[x+1][y])
+				pos[x+1][y].setCandidate(true);
+		}
+		if(y > 0 && !pos[x][y-1].getHaveCard()){ // Bottom
+			if(hSideCTS[x][y])
+				pos[x][y-1].setCandidate(true);
+		}
+		if(y < 4 && !pos[x][y+1].getHaveCard()){ // Top
+			if(hSideCTS[x][y+1])
+				pos[x][y+1].setCandidate(true);
+		}
+
+		if(x > 0 && pos[x-1][y].getHaveCard()) 
+			spread(x-1, y);
+		if(x < 8 && pos[x+1][y].getHaveCard()) 
+			spread(x+1, y);
+		if(x == 8 && (y == 1 || y == 3)
+			&& pos[x+1][y].getHaveCard())
+			spread(x+1, y);
+		if(x < 9 && y > 0 && pos[x][y-1].getHaveCard())
+			spread(x, y-1);
+		if(x < 9 && y < 4 && pos[x][y+1].getHaveCard())
+			spread(x, y+1);
+	}
+
+	public boolean breakRoad(int x, int y){
+		/* check position index */
+		if(x >= 10 || x < 0 || y >= 5 || y < 0){
+			System.out.println("No such position");
+			return false;
+		}
+		/* check haveCard */
+		if(!pos[x][y].getHaveCard()){
+			System.out.println("No road");
+			return false;
+		}
+		pos[x][y].removeCard();
+		/* reset CTS */
+		for(int i = 0; i < 10; i++){
+			for(int j = 0; j < 5; j++){
+				if(pos[i][j].getHaveCard()){
+					vSideCTS[i][j] = false;
+					vSideCTS[i+1][j] = false;
+					hSideCTS[i][j] = false;
+					hSideCTS[i][j+1] = false;
+				}
+			}
+		}
+		vSideCTS[1][2] = true; // Left 
+		vSideCTS[2][2] = true; // Right
+		hSideCTS[1][2] = true; // Bottom
+		hSideCTS[1][3] = true; // Top
+		/* reset candidates */
+		for(int i = 0; i < 10; i++){
+			for(int j = 0; j < 5; j++){
+				pos[i][j].setCandidate(false);
+			}
+		}
+		traceInit();
+		spread(1, 2); // spread from source
+
+		return true;
+	}
+	public char[][] MapStatus(){
+		char[][] simpleMap = new char[41][21];
+		for(int i = 0; i < 41; i ++)
+			for(int j = 0; j < 21; j ++)
+				simpleMap[i][j] = (i % 2 == 0 && (j % 2 == 0) && !((i + j) % 4 == 0)) ? '.':' ';
+			
+		simpleMap[4 * 1 + 2][4 * 2 + 2] = 's';
+		simpleMap[4 * 9 + 2][4 * 0 + 2] = 'b';
+		simpleMap[4 * 9 + 2][4 * 2 + 2] = 'm';
+		simpleMap[4 * 9 + 2][4 * 4 + 2] = 't';
+
+		for(int i = 0; i < 10; i ++)
+			for(int j = 0; j < 5; j ++){
+				if(!pos[i][j].getHaveCard())
+					continue;
+				if(pos[i][j].getIsDestination())
+					continue;
+				if(!pos[i][j].getBindToAny())
+					continue;
+				System.out.println(i +" " + j);
+				int centerx = i * 4 + 2;
+				int centery = j * 4 + 2;
+				if(pos[i][j].getConnect(0)){
+					simpleMap[centerx - 1][centery + 1] = '/';
+				}
+				if(pos[i][j].getConnect(1)){
+					simpleMap[centerx - 1][centery - 1] = '\\';
+				}
+				if(pos[i][j].getConnect(2)){
+					simpleMap[centerx + 1][centery - 1] = '/';
+				}
+				if(pos[i][j].getConnect(3)){
+					simpleMap[centerx + 1][centery + 1] = '\\';
+				}
+				if(pos[i][j].getConnect(4)){
+					simpleMap[centerx][centery + 1] = '|';
+					simpleMap[centerx][centery - 1] = '|';
+				}
+				if(pos[i][j].getConnect(5)){
+					simpleMap[centerx - 1][centery] = '-';
+					simpleMap[centerx + 1][centery] = '-';
+				}
+				if(!pos[i][j].getBind(0))
+					simpleMap[centerx][centery + 2] = 'x';
+				if(!pos[i][j].getBind(1))
+					simpleMap[centerx - 2][centery] = 'x';
+				if(!pos[i][j].getBind(2))
+					simpleMap[centerx][centery - 2] = 'x';
+				if(!pos[i][j].getBind(3))
+					simpleMap[centerx + 2][centery] = 'x';
+				if(pos[i][j].getIsBlock())
+					simpleMap[centerx][centery] = 'b';
+
+		}
+		return simpleMap;
+	}
+	public String toString(){
+		char[][] charMap = MapStatus();
+		String toReturn = new String();
+		for(int j = 20; j >= 0; j--){
+			toReturn += (j % 4 == 2) ? Integer.toString(j/4):" ";
+			for(int i = 0; i < 41; i ++){
+				toReturn += charMap[i][j];
+			}
+			toReturn += "\n";
+		}
+		for(int i = 0; i < 42; i ++){
+			toReturn += (i % 4 == 3)? Integer.toString(i/4):" ";
+		}
+		return toReturn;
+	}
+} 
